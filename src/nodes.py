ca2c0812f76bf2a929a42cca5220091a73faed41
maopeng <maopeng@asiainfo-data.com~>
2025-12-26 09:50:37 +0800

    add  remote node support

--- src/nodes.py
+++ src/nodes.py
@@ -1,4 +1,5 @@
 from .cloud.aliyun_oss_uploader import *
+from .remote_flux1 import RemoteFlux1Generator
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
@@ -6,11 +7,13 @@ from .cloud.aliyun_oss_uploader import *
 NODE_CLASS_MAPPINGS = {
     "AliyunOSSImageUploader": AliyunOSSImageUploader,
     "AliyunOSSVideoUploader": AliyunOSSVideoUploader,
-    "AliyunOSSAudioUploader": AliyunOSSAudioUploader
+    "AliyunOSSAudioUploader": AliyunOSSAudioUploader,
+    "RemoteFlux1Generator": RemoteFlux1Generator
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "AliyunOSSImageUploader": "阿里云OSS图片上传",
     "AliyunOSSVideoUploader": "阿里云OSS视频上传",
-    "AliyunOSSAudioUploader": "阿里云OSS音频上传"
+    "AliyunOSSAudioUploader": "阿里云OSS音频上传",
+    "RemoteFlux1Generator": "远程Flux1图像生成"
 }
